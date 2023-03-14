448ad0503d06b94cb7ad76dad35c581ae9eb2a19
Fernan Pietrantuono <fernanpietrantuono@gmail.com>
2023-03-14 17:16:34 -0300

    Tercer Commit

--- Ejercicios Extra/src/ejerciciosextra/Ejercicio16.java
+++ Ejercicios Extra/src/ejerciciosextra/Ejercicio16.java
@@ -9,20 +9,20 @@ public class Ejercicio16 {
         int edad = 0;
         mostrarInfo(nombre, edad);
     }
-    public static void mostrarInfo(String nombre, int edad){
+
+    public static void mostrarInfo(String nombre, int edad) {
         Scanner leer = new Scanner(System.in);
         String resp = "";
         boolean mayorDeEdad;
-        do{
+        do {
             System.out.println("Ingrese tu nombre");
             nombre = leer.next();
             System.out.println("Ingrese tu edad");
             edad = leer.nextInt();
-            mayorDeEdad = (edad>=18);
-            System.out.println(nombre+" de "+edad+" años, es mayor de edad? "
-            +mayorDeEdad);
+            mayorDeEdad = (edad >= 18);
+            System.out.println(nombre + " de " + edad + " años, es mayor de edad? " + mayorDeEdad);
             System.out.println("¿Quiere seguir mostrando personas?");
             resp = leer.next();
-        }while(!("no".equalsIgnoreCase(resp)));
+        } while (!("no".equalsIgnoreCase(resp)));
     }
 }
