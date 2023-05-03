05afa44f6d792182ba2d3dd35cba5c5cf26d6587
Fernan Pietrantuono <fernanpietrantuono@gmail.com>
2023-05-03 15:47:01 -0300

    Trigésimo Tercer Commit

--- POOConColecciones - Ejercicios/src/Servicios/Ejercicio6TiendaServicio.java
+++ POOConColecciones - Ejercicios/src/Servicios/Ejercicio6TiendaServicio.java
@@ -1,20 +1,19 @@
 package Servicios;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
 public class Ejercicio6TiendaServicio {
 
     private Scanner leer = new Scanner(System.in).useDelimiter("\n");
-    public final HashMap<String, Double> hm = new HashMap();
+    public final HashMap<String, Double> producto = new HashMap();
 
     public void introducirElemento() {
         System.out.println("Introduzca el nombre del producto");
-        String nom = leer.next();
+        String nombre = leer.next();
         System.out.println("Ahora el precio por favor");
         double precio = leer.nextDouble();
-        hm.put(nom, precio);
+        producto.put(nombre, precio);
     }
 
     public void modificarPrecio() {
@@ -22,14 +21,14 @@ public class Ejercicio6TiendaServicio {
         String busqueda = leer.next();
         System.out.println("Introduzca el valor nuevo del producto");
         double precio = leer.nextDouble();
-        hm.put(busqueda, precio);
+        producto.put(busqueda, precio);
     }
 
     public void eliminarProducto() {
         System.out.println("Introduzca un producto a borrar");
         String busqueda = leer.next();
-        if (hm.containsKey(busqueda)) {
-            hm.remove(busqueda);
+        if (producto.containsKey(busqueda)) {
+            producto.remove(busqueda);
             System.out.println("Producto eliminado");
         } else {
             System.out.println("El producto no existe");
@@ -37,20 +36,22 @@ public class Ejercicio6TiendaServicio {
     }
 
     public void mostrarProductos() {
-        for (Map.Entry<String, Double> aux : hm.entrySet()) {
+        producto.entrySet().forEach((aux) -> {
             System.out.println("Nombre: " + aux.getKey() + "\nPrecio: $" + aux.getValue());
-        }
+        });
     }
 
     public void menu() {
         boolean salir = false;
+        System.out.println("¡Bienvenido!");
         while (!salir) {
-            System.out.println("Bienvenido!");
+            System.out.println("MENÚ: ");
             System.out.println("\t° 1 - Introducir Producto");
             System.out.println("\t° 2 - Modificar Precio");
             System.out.println("\t° 3 - Eliminar Producto");
             System.out.println("\t° 4 - Mostrar Productos");
             System.out.println("\t° 5 - Salir");
+            System.out.println("Ingrese una opción");
             int choice = leer.nextInt();
             switch (choice) {
                 case 1:
@@ -66,7 +67,7 @@ public class Ejercicio6TiendaServicio {
                     mostrarProductos();
                     break;
                 case 5:
-                    System.out.println("Hasta Luego!");
+                    System.out.println("¡Hasta Luego!");
                     salir = true;
                     break;
                 default:
